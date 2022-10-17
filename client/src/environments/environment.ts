9c0bae63b85dfae0338818af64ef770d04681202
ygao <764714864@qq.com>
2022-10-17 15:56:57 -0400

    local deployment without ssl

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiURL: 'https://localhost:5001/api/',
+  apiURL: 'http://localhost:5000/api/',
   hubUrl: 'https://localhost:5001/hubs/',
 };
 
